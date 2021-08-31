e840da41764dec7c616e71a4b22e4c93c56d7628
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-08-31 14:27:32 +0530

    Create Code.py

---
+++ ARRAYS/Build Array from Permutation/Code.py
@@ -0,0 +1,8 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/build-array-from-permutation/
+*/
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+            return [nums[nums[i]] for i in range(len(nums))]
+            
