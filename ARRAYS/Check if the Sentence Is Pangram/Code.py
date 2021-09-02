a14128ed6acad930954c25455b8d8548eb1a1c07
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-02 17:34:02 +0530

    Create Code.py

---
+++ ARRAYS/Check if the Sentence Is Pangram/Code.py
@@ -0,0 +1,5 @@
+# PROBLEM LINK:- https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        return len(set(sentence)) == 26
