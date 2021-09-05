c5e1f5902bcd7fae697e3bbffed19e8dd2889774
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-05 11:40:21 +0530

    Create Code.py

---
+++ ARRAYS/Find Numbers with Even Number of Digits/Code.py
@@ -0,0 +1,13 @@
+# PROBLEM LINK:- https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        d = 0
+        for i in nums:
+            c = 0
+            while i > 0:
+                i //= 10
+                c += 1
+            if c % 2 == 0:
+                d += 1
+        return d
