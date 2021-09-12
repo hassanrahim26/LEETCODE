0a040b97b2c3ff0fff8ed5ab1c415c31d9cc8bd8
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-12 12:52:00 +0530

    Delete ARRAYS/Create Target Array in the Given Order directory

--- ARRAYS/Create Target Array in the Given Order/Code.py
+++
@@ -1,9 +0,0 @@
-# PROBLEM LINK:- https://leetcode.com/problems/create-target-array-in-the-given-order/
-
-class Solution:
-    def createTargetArray(self, v1, v2):
-        n = len(v2)
-        r = []
-        for i in range(0,n):
-            r.insert(v2[i], v1[i])
-        return r
