61641061061853fce3faaa097fdd0a7c3433ca07
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-12 12:44:50 +0530

    Create Code.cpp

---
+++ ARRAYS/Easy/Build Array from Permutation/Code.cpp
@@ -0,0 +1,16 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/build-array-from-permutation/
+*/
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+      int n = nums.size();
+      vector<int> ans(n);
+        for(int i = 0; i < n; i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
