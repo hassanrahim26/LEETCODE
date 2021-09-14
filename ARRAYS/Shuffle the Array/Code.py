756313aabad98073a852ce553e0c6637c991b0a1
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-14 10:39:38 +0530

    Delete ARRAYS/Shuffle the Array directory

--- ARRAYS/Shuffle the Array/Code.py
+++
@@ -1,9 +0,0 @@
-#PROBLEM LINK:- https://leetcode.com/problems/shuffle-the-array/
-
-class Solution:
-    def shuffle(self, nums, n): 
-        v = []
-        for i in range(n):
-            v.append(nums[i])
-            v.append(nums[i+n])
-        return v
