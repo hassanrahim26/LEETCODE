f895e265e0d71466f4fe58625c368431208d5704
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-14 10:38:20 +0530

    Delete ARRAYS/Running Sum of 1d Array directory

--- ARRAYS/Running Sum of 1d Array/Code.py
+++
@@ -1,7 +0,0 @@
-#PROBLEM LINK:- https://leetcode.com/problems/running-sum-of-1d-array/
-
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        for i in range(1,len(nums)):
-            nums[i] = nums[i-1]+nums[i]
-        return nums
