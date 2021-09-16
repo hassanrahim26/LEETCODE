8e0ed3ab98f916f98417e545c6159767541312e3
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-16 15:11:06 +0530

    Create Code.cpp

---
+++ SORTING/Easy/Majority Element/Code.cpp
@@ -0,0 +1,12 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/majority-element/
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size()    ;
+        sort(nums.begin(), nums.end());
+        return nums[n/2];
+    }
+};
