a82d10e102f116fed660d2c075e70b13b157ea69
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-19 14:36:39 +0530

    Create Code.cpp

---
+++ SEARCHING/EASY/Valid Perfect Square/Code.cpp
@@ -0,0 +1,16 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/valid-perfect-square/
+*/
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int i = 1;
+        while(num > 0)
+        {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+};
