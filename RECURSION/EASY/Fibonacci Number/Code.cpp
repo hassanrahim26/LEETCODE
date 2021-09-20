9719ed431e7bb333b0c9864f7ea5bf7b4b003308
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-20 19:37:14 +0530

    Create Code.cpp

---
+++ RECURSION/EASY/Fibonacci Number/Code.cpp
@@ -0,0 +1,21 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/fibonacci-number/
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)
+        {
+            return 0;    
+        }
+        
+        else if(n == 1)
+        {
+            return 1;
+        }
+        
+        int sum = fib(n-1) + fib(n-2);
+        return sum;
+    }
+};
