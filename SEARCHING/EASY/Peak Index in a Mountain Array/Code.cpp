9ff80820d4a254ac0628b0eb32f8e68de02b0424
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-21 15:22:15 +0530

    Create Code.cpp

---
+++ SEARCHING/EASY/Peak Index in a Mountain Array/Code.cpp
@@ -0,0 +1,20 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/peak-index-in-a-mountain-array/
+*/
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0, high = arr.size() - 1;
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            
+            if(arr[mid] < arr[mid+1])
+                low = mid + 1;
+            else
+                high = mid - 1;       
+        }
+        return low;
+    }
+};
