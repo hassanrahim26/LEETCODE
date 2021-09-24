a8b0d550230d77a8e5e48cf1a95dc42500b0c4c8
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-24 10:57:35 +0530

    Create Code.py

---
+++ SEARCHING/EASY/Binary Search/Code.py
@@ -0,0 +1,16 @@
+# PROBLEM LINK:- https://leetcode.com/problems/binary-search/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while(low <= high):
+            mid = int(low + (high - low)/2)
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
