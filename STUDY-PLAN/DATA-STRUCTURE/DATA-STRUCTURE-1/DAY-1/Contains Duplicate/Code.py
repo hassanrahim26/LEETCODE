5383dcd4cda5a600ab7831797979ea19d61a1b59
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-28 12:52:04 +0530

    Create Code.py

---
+++ STUDY-PLAN/DATA-STRUCTURE/DATA-STRUCTURE-1/DAY-1/Contains Duplicate/Code.py
@@ -0,0 +1,10 @@
+# PROBLEM LINK:- https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i-1] == nums[i]:
+                return True
+        return False
+            
