80dc63c5ea8036a114465e00402cd1f7a90893d2
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-09-28 12:48:11 +0530

    Update Code.cpp

--- STUDY-PLAN/DATA-STRUCTURE/DATA-STRUCTURE-1/DAY-1/Contains Duplicate/Code.cpp
+++ STUDY-PLAN/DATA-STRUCTURE/DATA-STRUCTURE-1/DAY-1/Contains Duplicate/Code.cpp
@@ -4,8 +4,13 @@ PROBLEM LINK:- https://leetcode.com/problems/contains-duplicate/
 
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& v) {
-        unordered_set<int> v_set(v.begin(), v.end());
-        return v_set.size() != v.size();
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i-1] == nums[i])
+                return true;
+        }
+        return false;
     }
 };
