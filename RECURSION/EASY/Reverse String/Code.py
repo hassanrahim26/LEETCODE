261d52d47acd7a670467b9fe5474ff401a0b6554
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-10-08 12:04:05 +0530

    Update Code.py

--- RECURSION/EASY/Reverse String/Code.py
+++ RECURSION/EASY/Reverse String/Code.py
@@ -2,9 +2,10 @@
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        start = 0
-        end = len(s) - 1
-        while(start < end):
-            s[start], s[end] = s[end], s[start]
-            start += 1
-            end -= 1
+        def reverse(a, b, s):
+            if a >= b:
+                return
+            else:
+                s[a], s[b] = s[b], s[a]
+                reverse(a+1, b-1, s)
+        reverse(0, len(s) - 1, s)
