f95f76033029444aa2f118216a97a32cf899860f
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-10-08 12:14:34 +0530

    Create Code.py

---
+++ RECURSION/EASY/Number of Steps to Reduce a Number to Zero/Code.py
@@ -0,0 +1,14 @@
+# PROBLEM LINK:- https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        c = 0
+        d = 0
+        while(num > 0):
+            if num % 2 == 0:
+                num /= 2
+                c+=1
+            else:
+                num -= 1
+                d+=1
+        return c + d
