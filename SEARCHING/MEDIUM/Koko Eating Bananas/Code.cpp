a424bf8b9425017d0c958345669a9188d41fc6a7
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-10-13 13:54:32 +0530

    Create Code.cpp

---
+++ SEARCHING/MEDIUM/Koko Eating Bananas/Code.cpp
@@ -0,0 +1,21 @@
+/*
+PROBLEM LINK:-
+*/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = 1000000000;
+        while(l < r)
+        {
+            int m = l + (r-l)/2, total = 0;
+            for(int p : piles)
+                total += (p + m - 1) / m;
+            if(total > h)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
+    }
+};
