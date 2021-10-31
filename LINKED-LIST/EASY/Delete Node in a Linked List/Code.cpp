262588b6b652cbca532253aeecd47ee8d64f16c6
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-10-31 13:49:30 +0530

    Create Code.cpp

---
+++ LINKED-LIST/EASY/Delete Node in a Linked List/Code.cpp
@@ -0,0 +1,13 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/delete-node-in-a-linked-list/
+*/
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node -> next;
+        node -> val = temp -> val;
+        node -> next = temp -> next;
+        delete temp;
+    }
+};
