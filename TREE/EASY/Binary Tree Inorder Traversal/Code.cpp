82c3f2f5fccc7f0ad1de1236ecad9ce8b491f474
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-11-26 19:52:19 +0530

    Create Code.cpp

---
+++ TREE/EASY/Binary Tree Inorder Traversal/Code.cpp
@@ -0,0 +1,23 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/binary-tree-inorder-traversal/
+*/
+
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& v)
+    {
+        if(!root)    
+            return;
+        
+        inorder(root -> left, v);
+        v.push_back(root -> val);
+        inorder(root -> right, v);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        inorder(root, v);
+        return v;
+    }
+    
+};
