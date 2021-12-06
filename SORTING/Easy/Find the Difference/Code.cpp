69cdcc903be37f039494886b0091f50ca2c4cdd7
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-12-06 14:56:15 +0530

    Create Code.cpp

---
+++ SORTING/Easy/Find the Difference/Code.cpp
@@ -0,0 +1,13 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/find-the-difference/
+*/
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char r = 0;
+        for(char c: s) r ^= c;
+        for(char c: t) r ^= c;  
+        return r;
+    }
+};
