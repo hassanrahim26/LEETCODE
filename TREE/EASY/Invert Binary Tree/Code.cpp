d66a900b9958b86a7d937f265581de28b042475b
HASSAN RAHIM <hassanrahim26@gmail.com>
2021-12-28 15:36:09 +0530

    Create Code.cpp

---
+++ TREE/EASY/Invert Binary Tree/Code.cpp
@@ -0,0 +1,17 @@
+/*
+PROBLEM LINK:- https://leetcode.com/problems/invert-binary-tree/
+*/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)        
+            return root;
+        
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+    }
+};
