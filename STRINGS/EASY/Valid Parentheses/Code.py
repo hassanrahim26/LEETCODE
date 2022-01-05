7fb87152fcfe7230338280b02f2269b5f263b682
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-01-05 16:02:11 +0530

    Create Code.py

---
+++ STRINGS/EASY/Valid Parentheses/Code.py
@@ -0,0 +1,15 @@
+# PROBLEM LINK:- https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
