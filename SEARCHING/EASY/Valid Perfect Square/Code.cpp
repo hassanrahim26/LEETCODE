bffd6ea291971a41310d7dd0b61fe7f2d030c502
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-04-18 20:48:00 +0530

    Update Code.cpp

--- SEARCHING/EASY/Valid Perfect Square/Code.cpp
+++ SEARCHING/EASY/Valid Perfect Square/Code.cpp
@@ -5,12 +5,20 @@ PROBLEM LINK:- https://leetcode.com/problems/valid-perfect-square/
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int i = 1;
-        while(num > 0)
+        int start = 1, end = num;
+        
+        while(start <= end)
         {
-            num -= i;
-            i += 2;
+            long long int mid = start + (end - start)/2;
+            
+            if(mid * mid == num)
+                return true;
+            else if(mid * mid < num)
+                start = mid + 1;
+            else
+                end = mid - 1;
         }
-        return num == 0;
+        
+        return false;
     }
 };
