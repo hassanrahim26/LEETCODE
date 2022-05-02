5db318511469cbec88f94370f6e5802dec43ec47
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-05-02 14:35:53 +0530

    Update Code.py

--- SEARCHING/MEDIUM/Find the Duplicate Number/Code.py
+++ SEARCHING/MEDIUM/Find the Duplicate Number/Code.py
@@ -2,6 +2,19 @@
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        while nums[0] != nums[nums[0]]:
-            nums[nums[0]], nums[0] = nums[0], nums[nums[0]]
-        return nums[0]
+        low, high = 0, len(nums)-1
+        
+        while low <= high:
+            mid = low + (high - low)//2
+            c = 0
+            
+            for x in nums:
+                if x <= mid:
+                    c += 1
+            
+            if c <= mid:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return low 
