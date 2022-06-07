a85de995af9a96893c332b3bb7a4699e7aafabdb
HASSAN RAHIM <hassanrahim26@gmail.com>
2022-06-07 13:52:25 +0530

    Update Code.cpp

--- SORTING/Easy/Merge Sorted Array/Code.cpp
+++ SORTING/Easy/Merge Sorted Array/Code.cpp
@@ -5,11 +5,14 @@ PROBLEM LINK:- https://leetcode.com/problems/merge-sorted-array/
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = 0; i < n; i++)
-        {
-            nums1[i + m] = nums2[i];
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while(i >= 0 and j >= 0){
+            if(nums1[i] < nums2[j])
+                nums1[k--] = nums2[j--];
+            else
+                nums1[k--] = nums1[i--];
         }
-        
-        sort(nums1.begin(), nums1.end());
+        while(j >= 0)
+            nums1[k--] = nums2[j--];
     }
 };
