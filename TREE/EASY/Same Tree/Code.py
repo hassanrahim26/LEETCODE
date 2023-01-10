323e5f9ee6c054a762f0d6718d43164fee89a745
Hassan Rahim <hassanrahim26@gmail.com>
2023-01-10 16:17:53 +0530

    Delete TREE/EASY/Same Tree directory

--- TREE/EASY/Same Tree/Code.py
+++
@@ -1,8 +0,0 @@
-# PROBLEM LINK:- https://leetcode.com/problems/same-tree/
-
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None or q == None:
-            return p == q
-        
-        return (p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
